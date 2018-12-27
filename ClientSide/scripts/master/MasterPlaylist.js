d264d6fb3acdedd649a1a08add1ce60a4ba78c4c
fstreun <fabiostr@bluewin.ch>
2018-12-27 21:07:06 +0100

    implemented Master Playlist

--- ClientSide/scripts/master/MasterPlaylist.js
+++ ClientSide/scripts/master/MasterPlaylist.js
@@ -1,47 +1,145 @@
 
 
 "use strict";
-const MasterPlayer = function() {
+const MasterPlaylist = function () {
+    let remote = null;
 
-    function forwardClicked(){
-        playlist.forward();
+    function setRemote(newRemote) {
+        remote = newRemote;
     }
 
-    function backwardClicked(){
-        playlist.backward();
+    const dataMap = new Map();  // maps itemId to video data
+
+    const playlistOrder = new Array();  // array of ordered itemIds
+    let current = -1;
+
+    let itemIdCounter = 0;
+
+    function forwardClicked() {
+        if (current < playlistOrder.length - 1) {
+            current++;
+
+            let itemId = playlistOrder[current];
+
+            let data = dataMap.get(itemId);
+
+            PlayerController.loadVideo(data, itemId)
+        } else {
+            // do nothing
+        }
     }
 
-    function cueAt(data, position){
-        playlist.cueAt(data, position);
+    function backwardClicked() {
+        if (current > 0) {
+            current--;
+
+            let itemId = playlistOrder[current];
+
+            let data = dataMap.get(itemId);
+
+            PlayerController.loadVideo(data, itemId)
+        } else {
+            // do nothing
+        }
     }
 
-    function cueAfterCurrent(data){
-        playlist.cueAt(data, current+1);
+    function cueAt(data, position) {
+        const itemId = itemIdCounter++;
+
+        playlistOrder.splice(position, 0, itemId);
+        dataMap.set(itemId, data);
+
+        const beforeId = playlistOrder[position + 1];
+
+        PlayerController.addBefore(itemdId, data, beforeId)
+        if (remote) {
+            remote.addBefore(itemId, data, beforeId);
+        }
     }
 
-    function cueEnd(data){
-        playlist.cueAt(data, playlistOrder.length);
+    function cueAfterCurrent(data) {
+        const itemId = itemIdCounter++;
+        const position = current + 1;
+
+        const currentId = playlistOrder[current];
+
+
+        playlistOrder.splice(position, 0, itemId);
+        dataMap.set(itemId, data);
+
+        PlayerController.addAfter(itemId, data, currentId); // returns undefined and not null
+        if (remote) {
+            remote.addAfter(itemId, data, currentId);
+        }
     }
 
-    function playVideoClicked(itemId){
-        playlist.playVideo(itemId)
+    function cueEnd(data) {
+        const itemId = itemIdCounter++;
+
+        playlistOrder.push(itemId);
+        dataMap.set(itemId, data)
+
+        PlayerController.addBefore(itemId, data, null);
+        if (remote) {
+            remote.addAfter(itemId, data, null);
+        }
     }
 
-    function removeVideoClicked(itemId){
-        playlist.remove(itemId);
+    function playVideoClicked(itemId) {
+        current = playlistOrder.indexOf(itemId);
+
+        PlayerController.loadVideo(dataMap.get(itemId), itemId);
     }
 
-    function movePerfomed(itemId, newPosition){
-        playlist.move(itemId, newPosition);
+    function removeVideoClicked(itemId) {
+        PlayerController.removeVideo(itemId);
+        if (remote) {
+            remote.removeVideo(itemId);
+        }
+
+        const position = playlistOrder.indexOf(itemId);
+        playlistOrder.splice(position, 1);
+        dataMap.delete(itemId);
+        if (position < current) {
+            current--;
+        }
+    }
+
+    function moveVideoPerformed(itemId, overItemId) {
+        if (itemId != overItemId) {
+            const oldPosition = playlistOrder.indexOf(itemId);
+            const newPosition = playlistOrder.indexOf(overItemId);
+
+            playlistOrder.splice(oldPosition, 1);
+            playlistOrder.splice(newPosition, 0, itemId);
+
+            if (oldPosition > current && newPosition <= current) {
+                current++;
+            } else if (oldPosition < current && newPosition > current) {
+                current--;
+            } else if (oldPosition == current) {
+                current = newPosition;
+            }
+
+            const after = playlistOrder[newPosition+1];
+
+            PlayerController.moveVideoBefore(itemId, after)
+            if(remote){
+                remote.moveVideoBefore(itemId, after);
+            }
+        }
     }
 
 
     return {
-        forward,
-        backward,
+        setRemote,
+        forwardClicked,
+        backwardClicked,
         cueAt,
-        playVideo,
-        remove,
-        move
+        cueAfterCurrent,
+        cueEnd,
+        playVideoClicked,
+        removeVideoClicked,
+        moveVideoPerformed
     };
-};
+}();
